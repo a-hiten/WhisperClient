1ad1f613e8233eefb6eb0fd13247b11f576e7d56
a-hiten <n.ruri0301@gmail.com>
2025-05-03 21:49:23 +0900

    共通処理を追加

---
+++ app/src/main/java/MyApplication.kt
@@ -0,0 +1,32 @@
+
+import android.app.Application
+
+// １．Applicationクラスを継承する
+class MyApplication : Application() {
+
+    // ２．グローバル変数loginUserIdを文字列型で宣言して、空文字で初期化する
+    var loginUserId: String = ""        // 変更される想定だからvarにしている
+
+    // ３．グローバル変数apiUrlを文字列型で宣言して、各チームのAPIのホームディレクトリで初期化する
+    val  apiUrl: String = "http://click.ecc.ac.jp/ecc/whisperSystem/"
+
+    // ４．Applicationクラスを他のクラスから参照できるように companion object を追加する
+    companion object {
+        // ４－１．このApplicationクラスのインスタンス（=自分自身）を保存するための変数を用意する
+        // ※あとでgetInstance()からアクセスするために使う
+        private lateinit var instance: MyApplication    // 宣言（後で初期化される）
+        // ４－２．他の画面（Activityなど）からこのApplicationクラスにアクセスできるようにするための関数を追加する。
+        fun getInstance(): MyApplication {
+            return instance
+        }
+    }
+
+    // ５．アプリ起動時に一度だけ呼ばれる初期化処理を定義するため、onCreate() をオーバーライドする
+    override fun onCreate() {       // 初期化される
+        super.onCreate()
+        // ５－１．自分自身（Applicationクラス）をinstanceに入れる
+        instance = this
+    }
+}
+
+
